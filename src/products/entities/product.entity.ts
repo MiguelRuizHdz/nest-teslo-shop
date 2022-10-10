5522697de7bfab1639b42f03ae76dd0fd580f341
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-10 12:05:17 -0500

    Entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -8,5 +8,38 @@ export class Product {
     @Column('text', {
         unique: true,
     })
-    title2: string;
+    title: string;
+
+    @Column('numeric',{
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    size: string[];
+
+    @Column('text')
+    gender: string;
+
+    // tags
+    // images
+
 }
