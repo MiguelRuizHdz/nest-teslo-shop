d68473c6a4c40ac034d5c7b2ffa066b21dc2a85a
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-11 10:15:57 -0600

    Custom Guard y Custom Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UseGuards, Req, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { IncomingHttpHeaders } from 'http';
 
@@ -7,6 +7,7 @@ import { GetUser, RawHeaders } from './decorators';
 
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
 
 
 @Controller('auth')
@@ -33,7 +34,7 @@ export class AuthController {
     @RawHeaders() rawHeaders: string[],
     @Headers() headers: IncomingHttpHeaders
   ) {
-    console.log(request);
+    // console.log(request);
     // console.log({ user: request.user});
 
     return {
@@ -46,4 +47,17 @@ export class AuthController {
     }
   }
 
+  @Get('private2')
+  @SetMetadata('roles', ['admin','superuser'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User,
+
+  ) {
+
+    return {
+      ok: true,
+      user,
+    }
+  }
 }
---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,23 @@
+import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+
+  constructor(
+    // Para ver informacion de los decoradores y de la metadata
+    private readonly reflector: Reflector
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const validRoles: string[] = this.reflector.get('roles', context.getHandler() );
+
+    console.log({ validRoles });
+
+    return true;
+  }
+}
