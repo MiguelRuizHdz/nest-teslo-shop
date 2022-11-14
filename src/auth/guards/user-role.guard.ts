187ae90c49f7a3120b093a1d6111efbf3190c957
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-14 07:54:08 -0600

    Verificar rol del usuario

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -1,6 +1,7 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, BadRequestException, ForbiddenException } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -16,8 +17,22 @@ export class UserRoleGuard implements CanActivate {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler() );
 
-    console.log({ validRoles });
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
 
-    return true;
+    if ( !user )
+      throw new BadRequestException('User not found');
+
+    console.log({ userRoles: user.roles });
+
+    for (const role of user.roles) {
+      if ( validRoles.includes( role ) ) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${ user.fullName } need a valid role: [${ validRoles }]`
+    );
   }
 }
