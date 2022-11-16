0b0d9a67d1995881a51ad4f499b4bbcf284e65ff
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-16 15:52:54 -0600

    Insertar userId en los productos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,6 +8,7 @@ import { PaginationDto } from '../common/dtos/pagination.dto';
 
 import { validate as isUUID } from 'uuid';
 import { Product, ProductImage } from './entities';
+import { User } from '../auth/entities/user.entity';
 
 
 @Injectable()
@@ -27,14 +28,15 @@ export class ProductsService {
 
   ) {}
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user: User) {
     try {
       
       const { images = [], ...productDetails } = createProductDto;
 
       const product = this.productRepository.create({
         ...createProductDto,
-        images: images.map( image => this.productImageRepository.create({ url: image }) )
+        images: images.map( image => this.productImageRepository.create({ url: image }) ),
+        user,
       });
       await this.productRepository.save( product );
 
@@ -96,7 +98,7 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user: User) {
 
     const { images, ...toUpdate } = updateProductDto;
 
@@ -118,6 +120,8 @@ export class ProductsService {
           image => this.productImageRepository.create({ url: image })
         )
       }
+
+      product.user = user;
 
       await queryRunner.manager.save( product );
       // await this.productRepository.save( product );
