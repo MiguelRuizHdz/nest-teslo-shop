cab94ee7ce0ddec63f33efb2ed2ae3fdfff9bdef
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-16 16:50:59 -0600

    Check Auth Status

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,6 +67,13 @@ export class AuthService {
     // TODO: Retornar el JWT
   }
 
+  async checkAuthStatus( user: User ) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    };
+  }
+
   private getJwtToken( payload: JwtPayload ) {
 
     const token = this.jwtService.sign( payload );
