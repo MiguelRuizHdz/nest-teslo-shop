a2743e66d6a2a26b34e8c54f4c1f2d1b7f429b3c
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-17 08:05:41 -0600

    Tags, ApiProperty y ApiResponse

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
+
 import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
 
-
+@ApiTags('Files - Get and Upload')
 @Controller('files')
 export class FilesController {
   constructor(
