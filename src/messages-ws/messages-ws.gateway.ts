b4ede510a324beeb2ff9c21c82f4db10826a23ad
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-19 18:35:34 -0600

    Cliente - Clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,10 +1,12 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
+  @WebSocketServer() wss: Server;
+
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) {}
@@ -13,12 +15,12 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     // console.log('Cliente conectado: ', client.id);
     this.messagesWsService.registerClient( client );
 
-    console.log({ conectados: this.messagesWsService.getConnectedClients()  });
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
   }
   
   handleDisconnect(client: Socket) {
     // console.log('Cliente desconectado: ', client.id);
     this.messagesWsService.removeClient( client.id );
-    console.log({ conectados: this.messagesWsService.getConnectedClients()  });
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
   }
 }
