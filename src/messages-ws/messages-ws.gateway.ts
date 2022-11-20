28b6655fe44079a4a794c70e566d582b9c39002d
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-19 19:08:31 -0600

    Emitir Cliente - Escuchar Servidor

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -23,4 +24,10 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     this.messagesWsService.removeClient( client.id );
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
   }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient( client: Socket, payload: NewMessageDto ) {
+    console.log(client.id, payload );
+  }
+
 }
